f06414d89b9aedde528a8dcb872e9265ace605bd
Phoenizard <jiangxiaoyi2004@gmail.com>
2025-02-24 13:10:26 +0000

    Initialize weights and biases for Linear Regression model and change activation function to ReLU

--- model/LinearRegressionModel.py
+++ model/LinearRegressionModel.py
@@ -8,9 +8,14 @@ class LinearRegressionModel(nn.Module):
         self.fc1 = nn.Linear(40, 1000)
         self.fc2 = nn.Linear(1000, 1)
         self.div = 1000
+        # Initialize the weights and bias
+        nn.init.kaiming_uniform_(self.fc1.weight, a=0, mode='fan_in', nonlinearity='relu')
+        nn.init.constant_(self.fc1.bias, 0)
+        nn.init.kaiming_uniform_(self.fc2.weight, a=0, mode='fan_in', nonlinearity='relu')
+        nn.init.constant_(self.fc2.bias, 0)
 
     def forward(self, x):
-        x = torch.tanh(self.fc1(x))
+        x = torch.relu(self.fc1(x))
         x = self.fc2(x)
         x = x / self.div
         return x
